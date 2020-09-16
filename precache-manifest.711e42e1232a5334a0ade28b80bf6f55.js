a844ad4af09d1718e6ec8ee72c12d424ce6207b0
Carson Fischl <carson.fischl@carleton.ca>
2020-09-16 15:37:21 -0400

    Updates

---
+++ precache-manifest.711e42e1232a5334a0ade28b80bf6f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eee49c6051d2fd26875e7ad235dd8e5",
+    "url": "/ccsc/index.html"
+  },
+  {
+    "revision": "baa5fa47f1028668c63b",
+    "url": "/ccsc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10c99b31015a1fd8685d",
+    "url": "/ccsc/static/js/2.341221e9.chunk.js"
+  },
+  {
+    "revision": "fbe06bcc249d3af1b96e1e74b0ad1f88",
+    "url": "/ccsc/static/js/2.341221e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa5fa47f1028668c63b",
+    "url": "/ccsc/static/js/main.478cbcdb.chunk.js"
+  },
+  {
+    "revision": "e62226faf85f026e81c2",
+    "url": "/ccsc/static/js/runtime-main.3fd7ca1f.js"
+  }
+]);
